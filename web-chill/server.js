a081f64941f3e241855598bac32be2cb10ea6ff4
euge <eugenio.cavina@studio.unibo.it>
2019-01-28 15:46:01 +0100

    Add loading chill theory to lpaas on startup

--- web-chill/server.js
+++ web-chill/server.js
@@ -1,11 +1,34 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+const fs = require('fs');
+const axios = require('axios');
+
+const lpaasUrl = 'http://localhost:8080'
+const theoryPath = '/lpaas/theories/chill'
+const chillTheoryPath = 'src/chess.pl'
+const headers = {
+  'Content-Type':'text/plain',
+  'Accept': 'application/json'
+}
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 5000;
+fs.readFile(chillTheoryPath, 'utf8', function(err, parsed) {
+    if (err) throw err
+
+    let body = parsed.replace(/%.*/g, '').replace(/\n/g, ' ').trim()
+    
+    axios.post(lpaasUrl+theoryPath, body, {headers: headers},
+    ).then(function(response) {
+      console.log("Chill Theory Loaded to "+lpaasUrl)
+    }).catch(function(error) {
+      console.log(error)
+    })
+})
+
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('server started '+ port);
